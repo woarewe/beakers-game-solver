78a06a7768783c41a5a4ef8dcc4504fb04e747a5
woarewe <rostislav@hey.com>
2021-07-02 16:33:45 +0300

    Fix file loading

--- lib/colors_sort_solution.ex
+++ lib/colors_sort_solution.ex
@@ -1,13 +1,13 @@
 defmodule ColorsSortSolution do
   def init do
-    # {:ok, %{"capacity" => capacity, "beakers" => colors_list}} = Path.join(File.cwd!(), "example.yml") |> YamlElixir.read_from_file
-    # beakers = for colors <- colors_list do
-    #   portions = Enum.map colors, fn color -> %Portion{color: color} end
-    #   %Beaker{capacity: capacity, portions: portions}
-    # end
-    # Enum.with_index beakers, fn(beaker, index) ->
-    #   {index + 1, beaker}
-    # end
+    {:ok, %{"capacity" => capacity, "beakers" => colors_list}} = Path.join(File.cwd!(), "example.yml") |> YamlElixir.read_from_file
+    beakers = for colors <- colors_list do
+      portions = Enum.map colors, fn(%{"color" => color, "size" => size}) -> %Portion{color: color, size: size} end
+      %Beaker{capacity: capacity, portions: portions}
+    end
+    Enum.with_index beakers, fn(beaker, index) ->
+      {index + 1, beaker}
+    end
   end
   
   def run do
