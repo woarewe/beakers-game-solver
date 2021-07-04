6c3b4228f39f0b64f397edbaca1a759c388ca2af
woarewe <rostislav@hey.com>
2021-07-04 16:39:20 +0300

    Init commit

---
+++ lib/colors_sort_solution.ex
@@ -0,0 +1,18 @@
+defmodule ColorsSortSolution do
+  @moduledoc """
+  Documentation for `ColorsSortSolution`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ColorsSortSolution.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/colors_sort_solution_test.exs
@@ -0,0 +1,8 @@
+defmodule ColorsSortSolutionTest do
+  use ExUnit.Case
+  doctest ColorsSortSolution
+
+  test "greets the world" do
+    assert ColorsSortSolution.hello() == :world
+  end
+end
